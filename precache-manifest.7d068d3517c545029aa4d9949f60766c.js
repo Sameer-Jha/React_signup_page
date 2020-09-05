49fc4166addf9bc42bf41b160b69c1db6fbfac56
Sameer-Jha <sameer.btech.cs17@iiitranchi.ac.in>
2020-09-05 17:10:31 +0530

    Updates

---
+++ precache-manifest.7d068d3517c545029aa4d9949f60766c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143deec2bd7c62a7f71b0e4ce5c59a50",
+    "url": "/React_signup_page/index.html"
+  },
+  {
+    "revision": "ce5dfafb24e270b3d182",
+    "url": "/React_signup_page/static/css/main.225579ee.chunk.css"
+  },
+  {
+    "revision": "29ff6d768e6e593e73bf",
+    "url": "/React_signup_page/static/js/2.afcf14e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_signup_page/static/js/2.afcf14e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5dfafb24e270b3d182",
+    "url": "/React_signup_page/static/js/main.a80a2ab5.chunk.js"
+  },
+  {
+    "revision": "9f0830b2504acacec25d",
+    "url": "/React_signup_page/static/js/runtime-main.1f3db9a8.js"
+  }
+]);
